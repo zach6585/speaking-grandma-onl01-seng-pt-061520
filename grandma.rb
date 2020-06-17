2c682c19f02a5ed384bc8579dc185e4ca77f2a79
zachary mines <zmines@wesleyan.edu>
2020-06-17 21:42:04 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -3,7 +3,7 @@
 # Write a speak_to_grandma method.
 def speak_to_grandma(phrase)
   if phrase != phrase.upcase
-    "HUH?! SPEAK UP, SONNY"
+    "HUH?! SPEAK UP, SONNY!"
   elsif phrase == "I LOVE YOU GRANDMA!"
     "I LOVE YOU TOO PUMPKIN!"
   else
