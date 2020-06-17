f7e374b21304a8f5ddb2e92550bafd285ef4fabb
zachary mines <zmines@wesleyan.edu>
2020-06-17 21:42:19 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -7,7 +7,7 @@ def speak_to_grandma(phrase)
   elsif phrase == "I LOVE YOU GRANDMA!"
     "I LOVE YOU TOO PUMPKIN!"
   else
-    "NO, NOT SINCE 1938"
+    "NO, NOT SINCE 1938!"
   end
 end
 # Whatever you say to grandma, she should respond with
